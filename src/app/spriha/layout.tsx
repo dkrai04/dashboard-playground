b7d268dbca1da60a1f7f7171d43e3f7bd1341f03
dkrai04 <deepanshu.kumar@juspay.in>
2026-04-01 16:11:26 +0530

    cleanup

--- src/app/spriha/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { BlendShell } from "../(dashboard)/BlendShell";
-import type { LayoutProps } from "@/types";
-
-export default function SprihaLayout({ children }: LayoutProps) {
-  return <BlendShell>{children}</BlendShell>;
-}
